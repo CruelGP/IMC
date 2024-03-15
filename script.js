0c49a666211549f8e017b115347353dcc0adf07d
Gustavo Pamponet <106318703+CruelGP@users.noreply.github.com>
2024-03-15 10:58:21 -0300

    up

---
+++ script.js
@@ -0,0 +1,11 @@
+//Variáveis
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsumit = function(event){
+    event.preventDefault()
+    
+    const weight = inputHeight.value
+    const heught = inputHeight.value
+}
