d28b25b67a67a4a7bddc1a4f3e62469de74bc87e
Gustavo Pamponet <106318703+CruelGP@users.noreply.github.com>
2024-03-19 07:21:48 -0300

    up

--- script.js
+++ script.js
@@ -3,9 +3,26 @@ const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
 const inputHeight = document.querySelector('#height')
 
-form.onsumit = function(event){
+const modalWrapper = document.querySelector('.modal-whapper')
+const modalMessage = document.querySelector('.modal .title span')
+const modalBtnClose = document.querySelector('.modal buttton.close')
+
+
+form.onsumit = event => {
     event.preventDefault()
-    
-    const weight = inputHeight.value
-    const heught = inputHeight.value
+
+    const weight = inputWeight.value
+    const height = inputHeight.value
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    modalMessage.innerText = message
+    modalWrapper.classList.add('open')
+
+    console.log (weight, height)
+}
+
+function IMC (weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
